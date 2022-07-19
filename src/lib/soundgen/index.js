025220812e5a4bcf331d4c983b04d408563a6c98
Sage Raflik <neonfuz@gmail.com>
2022-07-19 12:09:40 -0500

    soundgen: moved Voice to own component

--- src/lib/soundgen/index.js
+++ src/lib/soundgen/index.js
@@ -1,3 +1,5 @@
+import Voice from './Voice.js';
+
 class WaveGenerator extends AudioWorkletProcessor {
     stop = false;
 
@@ -28,65 +30,3 @@ class WaveGenerator extends AudioWorkletProcessor {
 }
 
 registerProcessor('wave-generator', WaveGenerator);
-
-class Voice {
-    srate = 48000;
-    oscs = [];
-
-    constructor () {
-        this.oscs = [];
-    }
-
-    // Given an array of channels, write the voice into a buffer
-    writeWave (channels) {
-        channels.forEach(channel => {
-            const oscCache = [];
-            for (let i=0; i<channel.length; ++i) {
-                this.oscs.forEach((osc, i) => {
-                    oscCache[i] = osc.getSample();
-                    osc.modulation.forEach((depth, i) =>
-                        osc.modulateWith(oscCache[i], depth))
-                })
-                channel[i] = oscCache[this.oscs.length-1] * this.volume;
-            }
-        });
-    }
-
-    setSrate (srate) {
-        this.oscs.forEach((osc) => osc.srate = srate);
-    }
-
-    setParams ({numOscs, oscs, volume}) {
-        this.volume = volume;
-        if (this.oscs.length !== numOscs) {
-            this.oscs = Array(numOscs).fill().map(() => new SineWave());
-        }
-        oscs.forEach((osc, index) => {
-            const o = this.oscs[index];
-            o.setFrequency(osc.pitch);
-            o.modulation = osc.modulation;
-        });
-    }
-}
-
-class SineWave {
-    srate = 48000;
-    phase = 0;
-    phaseadd = 1;
-
-    constructor () {
-    }
-
-    setFrequency (pitch) {
-        this.phaseadd = Math.PI * 2.0 * pitch / this.srate;
-    }
-
-    getSample () {
-        this.phase += this.phaseadd;
-        return Math.sin(this.phase);
-    }
-
-    modulateWith (sample, modDepth) {
-        this.phase += sample * modDepth / this.srate * 100;
-    }
-}
