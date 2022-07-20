b51a818294d09dd31cd11e40cac481f46cc0162f
Sam Pagenkopf <ssaammp@gmail.com>
2022-07-19 23:01:56 -0500

    basic piano

---
+++ src/lib/piano/Piano.svelte
@@ -0,0 +1,54 @@
+<script>
+ export let basePitch;
+ let elemWidth = 800;
+ $: numKeys = Math.floor(elemWidth / 15);
+ $: firstOctave = 5 - Math.floor(numKeys / 12.0 / 2.0);
+ $: firstNote = firstOctave * 12;
+
+ function hitKey(index) {
+     basePitch = 440 * Math.pow(2, (index-60)/12)
+ }
+
+ function generateKeys (numKeys) {
+     function isWhiteNote(index) {
+         return [1,0,1,0,1,1,0,1,0,1,0,1][index % 12];
+     }
+     function collapseColumn(index) {
+         const octave = Math.floor(index / 12) * 7;
+         return [1,1,2,2,3,4,4,5,5,6,6,7][index % 12] + octave;
+     }
+     return Array(numKeys).fill().map((_, index) => {
+         let isWhite = isWhiteNote(index);
+         let row = isWhite ? 0 : 1;
+         let column = collapseColumn(index);
+         return {isWhite, row, column};
+     })
+ }
+</script>
+
+<div id="piano" bind:clientWidth="{elemWidth}">
+    {#each generateKeys(numKeys) as {isWhite, row, column}, index}
+        <button
+            style="grid-column: {column} / {column}; grid-row: {row} / {row}"
+            on:mousedown="{() => hitKey(firstNote + index)}"
+            class="{isWhite ? 'whiteKey' : 'blackKey'}"
+        >
+        </button>
+    {/each}
+</div>
+
+<style>
+ .whiteKey {
+     transform: translate(0%, -50%);
+     height: 200%;
+ }
+ .blackKey {
+     transform: translate(50%, 0%);
+     z-index: 1;
+ }
+ #piano {
+     grid-template-rows: 60px 50px;
+     grid-auto-flow: row;
+     display: grid;
+ }
+</style>
