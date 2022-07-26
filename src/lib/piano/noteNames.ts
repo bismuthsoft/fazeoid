3a9d0db11b93e0e5868288a6d8205c2685a7979a
Sam Pagenkopf <ssaammp@gmail.com>
2022-07-26 14:29:30 -0500

    key labels

---
+++ src/lib/piano/noteNames.ts
@@ -0,0 +1,16 @@
+const noteNames: string[] = [
+    'C',
+    'C♯',
+    'D',
+    'D♯',
+    'E',
+    'F',
+    'F♯',
+    'G',
+    'G♯',
+    'G',
+    'A♯',
+    'B',
+];
+
+export default noteNames;
