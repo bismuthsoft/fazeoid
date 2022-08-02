5d777b16a50360602ddd7faae1059fc0fad1b0a0
Sage Raflik <neonfuz@gmail.com>
2022-08-01 19:37:18 -0500

    Styled buttons to match theme a bit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,4 +5,10 @@
      margin: 0;
      font-family: sans-serif;
  }
+ :global(button) {
+     border: solid #333 .125rem;
+     padding: .3rem .5rem;
+     font-weight: bold;
+     border-radius: .5em;
+ }
 </style>
