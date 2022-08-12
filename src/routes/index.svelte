4f1428699c1cc00e4a671faf79223599ef84999d
Sam Pagenkopf <ssaammp@gmail.com>
2022-08-12 15:11:29 -0500

    turn index.svelte tabs into spaces

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,49 +12,49 @@
  let ctrl = new AudioController();
  let hackInitialize = false;
  instrument.subscribe((value) => {
-     ctrl.postMessage('setInstrument', 0, value);
+	 ctrl.postMessage('setInstrument', 0, value);
  });
 
  onDestroy(() => ctrl.stop());
 
  function noteUp (ev: CustomEvent) {
-     if (!hackInitialize) {
-         ctrl.audioContext?.resume();
-         hackInitialize = true;
-     }
-     ctrl.postMessage('noteUp', ev.detail);
+	 if (!hackInitialize) {
+		 ctrl.audioContext?.resume();
+		 hackInitialize = true;
+	 }
+	 ctrl.postMessage('noteUp', ev.detail);
  }
 
  function noteDown (ev: CustomEvent) {
-     if (!ctrl.started) {
-         ctrl.started = true;
-         ctrl.setupWorklet();
-     }
-     ctrl.postMessage('noteDown', ev.detail);
+	 if (!ctrl.started) {
+		 ctrl.started = true;
+		 ctrl.setupWorklet();
+	 }
+	 ctrl.postMessage('noteDown', ev.detail);
  }
 </script>
 
 <svelte:head>
-    <title>
-        FMSite
-    </title>
+	<title>
+		FMSite
+	</title>
 </svelte:head>
 
 <div class="container">
-    <InstrumentPanel bind:params="{$instrument}" on:noteUp="{noteUp}" on:noteDown="{noteDown}"/>
+	<InstrumentPanel bind:params="{$instrument}" on:noteUp="{noteUp}" on:noteDown="{noteDown}"/>
 </div>
 
 <Taskbar />
 
 <style>
  .container {
-     display: grid;
-     place-items: center;
-     height: 100vh;
-     width: 100vw;
+	 display: grid;
+	 place-items: center;
+	 height: 100vh;
+	 width: 100vw;
  }
  :global(body) {
-     background: url(https://www.newegg.com/insider/wp-content/uploads/windows_xp_bliss-wide.jpg);
-     background-size: cover;
+	 background: url(https://www.newegg.com/insider/wp-content/uploads/windows_xp_bliss-wide.jpg);
+	 background-size: cover;
  }
 </style>
