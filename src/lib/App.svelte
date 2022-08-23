aa45143baed1bc5c541ada0ff7f66497258e7031
Sam Pagenkopf <ssaammp@gmail.com>
2022-08-23 16:19:51 -0500

    futuristic background

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -57,8 +57,8 @@
      padding: 0.5rem;
      border-radius: 1em;
      background: var(--bg-color);
-     --bg-color: #8fa3;
-     backdrop-filter: brightness(0.5) contrast(0.8) hue-rotate(0.90turn) blur(5px);
+     --bg-color: #0008;
+     backdrop-filter: contrast(0.5) blur(5px);
      color: white;
      filter: drop-shadow(4px 4px 10px #3338);
  }
