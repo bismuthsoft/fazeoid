56c038b5cd9bcbedef6d809226754f7ce04bebc6
Sage Raflik <neonfuz@gmail.com>
2022-08-24 22:33:59 -0500

    Disabled client-router app-wide for cpu cycles

--- src/routes/+page.server.js
+++
@@ -1,2 +0,0 @@
-// Svelte-kit router adds handlers on pointermove which consume cpu
-export const router = false;
--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,11 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+        browser: {
+            // disabling router app-wide saves some cpu cycles
+            router: false
+        }
 	}
 };
 
