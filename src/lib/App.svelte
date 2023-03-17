ba8eb69a72d2e0c2485c3c61c5e61b57df994b74
Sam Pagenkopf <ssaammp@gmail.com>
2023-03-17 14:23:39 -0500

    use non-bold, non-italic font

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -54,9 +54,8 @@
   .App {
     font-family: Genos, Impact, Haettenschweiler, "Arial Narrow Bold",
       sans-serif;
-    font-weight: 700;
-    font-style: italic;
     font-display: block;
+    font-weight: 400;
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
@@ -80,29 +79,28 @@
     letter-spacing: 0.2rem;
     user-select: none;
   }
-  /* latin-ext */
-  @font-face {
-    font-family: "Genos";
-    size-adjust: 120%;
-    font-style: italic;
-    font-weight: 700;
-    font-display: swap;
-    src: url(https://fonts.gstatic.com/s/genos/v6/SlGPmQqPqpUOYRwqWzksdKTv0zsAYgvn6Xi3A7JMAg.woff2)
-      format("woff2");
-    unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB,
-      U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-  }
   /* latin */
   @font-face {
     font-family: "Genos";
     size-adjust: 120%;
-    font-style: italic;
-    font-weight: 700;
+    font-style: normal;
+    font-weight: 400;
     font-display: swap;
-    src: url(https://fonts.gstatic.com/s/genos/v6/SlGPmQqPqpUOYRwqWzksdKTv0zsAYgvn6Xi5A7I.woff2)
+    src: url(https://fonts.gstatic.com/s/genos/v6/SlGNmQqPqpUOYTYjacb0Hc91fTwVqkjYqki7.woff2)
       format("woff2");
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
       U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
       U+2215, U+FEFF, U+FFFD;
+  }
+  /* latin-ext */
+  @font-face {
+    font-family: "Genos";
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url(https://fonts.gstatic.com/s/genos/v6/SlGNmQqPqpUOYTYjacb0Hc91fTwVqkjYpEi7G7Y.woff2)
+      format("woff2");
+    unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB,
+      U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
   }
 </style>
