d0c15cff1ad9cbb6bf568b96ae369d2cc08e6f87
Sam Pagenkopf <ssaammp@gmail.com>
2024-07-22 00:56:48 -0500

    Revert "copy example mongodb api with user auth"
    This reverts commit 44cddbd76cfc8541b66809c4ff5ee8ae17f4a2c6.

--- api/src/exceptions/unauthorizedError.ts
+++
@@ -1,7 +0,0 @@
-import { CustomError } from './customError';
-
-export class UnauthorizedError extends CustomError {
-    constructor(message: string) {
-        super(message, 401);
-    }
-}
--- api/src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-export const ROLES = {
-    ADMIN: 'ADMIN',
-    USER: 'USER'
-};
